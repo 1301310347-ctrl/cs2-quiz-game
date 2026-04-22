3c9213e120edbd9f88b60d20bd08490f17efbdaf
Nathaniel E Adams <47441281+nadamspueblo@users.noreply.github.com>
2026-04-22 02:58:19 +0000

    Add TODO comments and removed code

--- script.js
+++ script.js
@@ -1,106 +1,54 @@
 console.log("Script started");
 
-let questions = [
-    {
-        question: "What is the capital of Arizona?",
-        option1: "Tucson",
-        option2: "Phoenix",
-        option3: "Yuma",
-        option4: "Flagstaff",
-        answer: "Phoenix"
-    },
-    {
-        question: "How many nickels in a quarter?",
-        option1: 25,
-        option2: 10,
-        option3: 5,
-        option4: 3,
-        answer: 5
-    },
-    {
-        question: "What gets wetter as it dries?",
-        option1: "Paint",
-        option2: "A dolphin",
-        option3: "Hair",
-        option4: "A towel",
-        answer: "A towel"
-    }
-]
+/* Questions objects should be formatted like this:
+{
+    question: "What is the capital of Arizona?",
+    option1: "Tucson",
+    option2: "Phoenix",
+    option3: "Yuma",
+    option4: "Flagstaff",
+    answer: "Phoenix"
+}
+*/
 
-let userAnswers = [];
+// TODO: Create an array of objects to store the questions, options, and answer
 
-let index = 0;
+// TODO: Declare an empty array that will hold the user's answers
+
+// TODO: Declare a variable to keep track of which question (index) we are on
 
 function showNextQuestion() {
-    if (index == questions.length) {
-        checkAnswers();
-        return;
-    }
-    let current = questions[index];
+    // TODO: Get the current question
     
-    let questionText = document.getElementById("question");
-    questionText.innerText = current.question;
+    // TODO: Set the text of the question element
 
-    let choice1 = document.getElementById("option1text");
-    choice1.innerText = current.option1;
-
-    let choice2 = document.getElementById("option2text");
-    choice2.innerText = current.option2;
-
-    let choice3 = document.getElementById("option3text");
-    choice3.innerText = current.option3;
-
-    let choice4 = document.getElementById("option4text");
-    choice4.innerText = current.option4;
+    // TODO: Set the text of each option element
 }
 
+// TODO: Call showNextQuestion to load the first question when the page loads
+
 function submitAnswer(event) {
-    event.preventDefault();
+    // TODO: Prevent the form from reloading the page
+
+    // Get the radio button input elements
     let option1 = document.getElementById("option1");
     let option2 = document.getElementById("option2");
     let option3 = document.getElementById("option3");
     let option4 = document.getElementById("option4");
 
-    let currentQ = questions[index];
-    if (option1.checked) {
-        userAnswers.push(currentQ.option1);
-        option1.checked = false;
-    }
-    else if (option2.checked) {
-        userAnswers.push(currentQ.option2);
-        option2.checked = false;
-    }
-    else if (option3.checked) {
-        userAnswers.push(currentQ.option3);
-        option3.checked = false;
-    }
-    else if (option4.checked) {
-        userAnswers.push(currentQ.option4);
-        option4.checked = false;
-    }
-    else {
-        alert("You must select an answer");
-        return;
-    }
+    // TODO: Get the current question
 
-    console.log(userAnswers);
-    index++;
-    showNextQuestion();
+    // TODO: Use a conditional (if/else-if) to check which option was clicked and push the option text into the userAnswer array
+
+    // TODO: Add one to the question index
+
+    // TODO: Load the next question
 }
 
 function checkAnswers() {
-    let numCorrect= 0;
-    for (let i = 0; i < userAnswers.length; i++) {
-        let userAns = userAnswers[i];
-        let question = questions[i];
-        if (userAns == question.answer) {
-            numCorrect++;
-        }
-    }
-    alert("You got " + numCorrect + " out of " + questions.length + " correct!");
-    index = 0;
-    userAnswers = [];
-    showNextQuestion();
-}
+    // TODO: Iterate through the user answers array and count how many are correct
 
-showNextQuestion();
+    // TODO: Show an alert to the user with how many they got right out of the total
+
+    // TODO: Reset and start over
+}
